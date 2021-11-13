e93ceb5da4f99c432cfee5bf5c5fba9ea2c356d9
John Maximilian <2e0byo@gmail.com>
2021-11-13 16:59:41 +0000

    handle numbers smaller than 100

--- yadc/humanlike.py
+++ yadc/humanlike.py
@@ -12,6 +12,11 @@ def randsleep(target: int, max_: int = None):
     than a linear.
     """
     max_ = max_ or target * 10
+    multiplier = 1
+    while target < 100:
+        multiplier *= 10
+        target *= multiplier
+        max_ *= multiplier
     duration = min(np.random.poisson(target), max_)
-    logger.debug(f"Sleeping for {duration} s")
-    sleep(duration)
+    logger.debug(f"Sleeping for {duration/multiplier} s")
+    sleep(duration / multiplier)
