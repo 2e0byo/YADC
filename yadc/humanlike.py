8524210d3c147981344f93e06d79b70b481b602f
John Maximilian <2e0byo@gmail.com>
2021-11-13 15:48:38 +0000

    default max

--- yadc/humanlike.py
+++ yadc/humanlike.py
@@ -5,12 +5,13 @@ from logging import getLogger
 logger = getLogger(__name__)
 
 
-def randsleep(target: int, max_: int = 400):
+def randsleep(target: int, max_: int = None):
     """Sleep for a random length of time around target but less than max.
 
     We use a poisson distribution, since that models human action a bit better
     than a linear.
     """
+    max_ = max_ or target * 10
     duration = min(np.random.poisson(target), max_)
     logger.debug(f"Sleeping for {duration} s")
     sleep(duration)
