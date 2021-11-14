ca57b557dc741e26798fa68f00c4d32386fd3e40
John Maximilian <2e0byo@gmail.com>
2021-11-14 21:16:11 +0000

    feat(main): demo main.py

---
+++ yadc/main.py
@@ -0,0 +1,36 @@
+import logging
+from datetime import datetime
+import coloredlogs
+from pathlib import Path
+
+from .browser import Browser, TorBrowser
+from .scraper import Driver, Scraper, Test, Centre
+
+logging.basicConfig(level=logging.DEBUG)
+coloredlogs.install()
+
+
+def notify(msg):
+    """Put notification code here."""
+    pass
+
+
+# define a driver.  You can define as many as you like.
+driver = Driver(
+    licence_number="YOUR_NUMBER",
+    booking_ref="YOUR_REF",
+    not_before=datetime(2021, 12, 10),
+    not_after=datetime(2022, 3, 1),
+    centres=[
+        Centre(centre="Gateshead", before=datetime(2022, 1, 1)),
+        Centre(centre="Durham"),
+    ],
+)
+
+# get a scraper for a driver or drivers.
+s = Scraper(
+    TorBrowser(buster=Path("buster"), chrome="chromium"), [driver], notify=notify
+)
+
+# run the scraper.  I'm a bit too addicted to callable objects...
+s()
