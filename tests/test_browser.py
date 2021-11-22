f426ff1852b6a212675358cfa20c55901c30643b
John Maximilian <2e0byo@gmail.com>
2021-11-22 18:02:02 +0000

    test: setup for browser testing.

--- tests/test_browser.py
+++ tests/test_browser.py
@@ -1,12 +1,59 @@
-import sys
-
-print(sys.path)
-
-from yadc.browser import Browser
+from yadc.browser import Browser, TorBrowser
+import pytest
+import shutil
 
 
-def test_browser_default():
+def executable_path(*tries):
+    """Find path to executable, or throw."""
+    path = None
+    for ex in tries:
+        path = shutil.which(ex)
+        if path:
+            break
+
+    if not path:
+        raise Exception(f"Unable to find path to {tries[0]}")
+
+    return path
+
+
+@pytest.fixture
+def chrome():
+    return executable_path("chrome", "chromium", "google-chrome-stable", "chrome.exe")
+
+
+@pytest.fixture
+def chromedriver():
+    return executable_path("chromedriver", "chromedriver.exe")
+
+
+@pytest.fixture
+def tor():
+    return executable_path("tor", "TOR/tor.exe")
+
+
+def smoke_test_browser():
     with Browser() as driver:
+        assert any(
+            (
+                "Driving license number" in driver.page_source,
+                "Queue-it" in driver.page_source,
+            )
+        )
+
+
+def test_paths_chrome_chromedriver(chrome, chromedriver):
+    with Browser(chrome=chrome, chromedriver=chromedriver) as driver:
+        assert any(
+            (
+                "Driving license number" in driver.page_source,
+                "Queue-it" in driver.page_source,
+            )
+        )
+
+
+def test_paths_tor_browser(chrome, chromedriver, tor):
+    with TorBrowser(chrome=chrome, chromedriver=chromedriver, tor=tor) as driver:
         assert any(
             (
                 "Driving license number" in driver.page_source,
