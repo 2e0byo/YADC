d9347598ce8f9050d8170ecc2d571b10bc91a33c
John Maximilian <2e0byo@gmail.com>
2022-01-21 17:35:56 +0000

    deps: bump tor version for windows install.

--- scripts/download_tor.py
+++ scripts/download_tor.py
@@ -2,7 +2,7 @@ import zipfile
 import io
 import requests
 
-TOR = "https://www.torproject.org/dist/torbrowser/11.0.1/tor-win32-0.4.6.8.zip"
+TOR = "https://www.torproject.org/dist/torbrowser/11.0.4/tor-win32-0.4.6.9.zip"
 r = requests.get(TOR)
 with zipfile.ZipFile(io.BytesIO(r.content)) as zf:
     zf.extractall()
