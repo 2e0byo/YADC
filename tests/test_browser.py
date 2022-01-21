979304d0d44f9f6b6f23a3af665c3253dc6c2e9d
John Maximilian <2e0byo@gmail.com>
2022-01-21 17:36:12 +0000

    tests: add test of error dumping.

--- tests/test_browser.py
+++ tests/test_browser.py
@@ -49,3 +49,16 @@ def test_paths_chrome_chromedriver_graphical(chrome, chromedriver):
 
 def test_paths_tor_browser_graphical(chrome, chromedriver, tor):
     run_test_browser(TorBrowser(chrome=chrome, chromedriver=chromedriver, tor=tor))
+
+
+def test_dump_errors(mocker, tmp_path):
+    mocker.patch.object(Browser, "__enter__")
+    b = Browser(errors_dir=tmp_path)
+    msg = "Test exception message"
+    with pytest.raises(Exception):
+        with b:
+            raise Exception(msg)
+    with next(tmp_path.glob("*.txt")).open() as f:
+        assert msg in f.read()
+
+    assert len(list(tmp_path.glob("*.html"))) == 1
