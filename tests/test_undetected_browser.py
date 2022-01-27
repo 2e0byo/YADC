eaa4efb243ee57764a4e7228d06614646e32fd53
John Maximilian <2e0byo@gmail.com>
2022-01-27 13:03:56 +0000

    tests: test for all browser objects.

--- tests/test_undetected_browser.py
+++ tests/test_undetected_browser.py
@@ -10,6 +10,11 @@ from yadc.undetected_browser import (
 )
 
 
+def pingtest(driver):
+    driver.get("https://www.google.com")
+    assert "Google Search" in driver.page_source
+
+
 def run_browser_test(browser):
     unique_url = (
         "chrome-extension://mpbjkejclgfgadiemmefgebjfooflfhl/src/options/index.html"
@@ -17,8 +22,7 @@ def run_browser_test(browser):
     with browser as driver:
         driver.get(unique_url)
         assert "Buster" in driver.page_source
-        driver.get("https://www.google.com")
-        assert "Google Search" in driver.page_source
+        pingtest(driver)
 
 
 @pytest.mark.graphical
@@ -58,9 +62,28 @@ def test_automated_manual_buster_undetected_tor_browser(tmp_path):
 
 
 @pytest.mark.graphical
-def test_automated_manual_buster_undetected_tor_browser(tmp_path):
-    shutil.copytree("tests/blank-profile", tmp_path / "blank-profile")
-    br = ManualBusterUndetectedTorBrowser(
-        buster=True, profile_dir=(tmp_path) / "blank-profile"
-    )
-    run_browser_test(br)
+def test_undetected_browser(tmp_path):
+    br = UndetectedBrowser()
+    with br as driver:
+        pingtest(driver)
+
+
+@pytest.mark.graphical
+def test_undetected_tor_browser(tmp_path):
+    br = UndetectedTorBrowser()
+    with br as driver:
+        pingtest(driver)
+
+
+@pytest.mark.graphical
+def test_buster_undetected_browser(tmp_path, buster):
+    br = UndetectedBrowser(buster=buster)
+    with br as driver:
+        pingtest(driver)
+
+
+@pytest.mark.graphical
+def test_buster_undetected_tor_browser(tmp_path, buster):
+    br = UndetectedTorBrowser(buster=buster)
+    with br as driver:
+        pingtest(driver)
