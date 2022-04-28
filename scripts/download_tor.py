54f5d1aec349eb6fe75950dab0b94f876cd4ce61
John Maximilian <2e0byo@gmail.com>
2022-04-28 15:20:12 +0100

    ci: attempt to make tor script more reliable

--- scripts/download_tor.py
+++ scripts/download_tor.py
@@ -1,8 +1,14 @@
-import zipfile
 import io
+import zipfile
+import re
 import requests
 
-TOR = "https://dist.torproject.org/torbrowser/11.0.7/tor-win64-0.4.6.10.zip"
-r = requests.get(TOR)
+DOWNLOAD_PAGE = "https://www.torproject.org/download/tor/"
+BASE = "https://www.torproject.org"
+
+data = requests.get(DOWNLOAD_PAGE).text
+url = re.search('"([a-zA-Z0-9./-]+\.zip)"', data).group(1)
+
+r = requests.get(BASE + url)
 with zipfile.ZipFile(io.BytesIO(r.content)) as zf:
     zf.extractall()
